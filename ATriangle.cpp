cb91e94bbc717a707f3e38b2a5b8b42d3ae8af08
maizuddin35 <maizuddin35@gmail.com>
2018-01-30 15:43:38 +0800

    Create ATriangle.cpp

---
+++ ATriangle.cpp
@@ -0,0 +1,22 @@
+//buat program untuk dapatkan value area triangle
+formula -> (1/2*x*y)
+
+#include<iostream>
+using namespace std;
+
+class CTriangle
+{
+private:
+int x,y;
+
+public:
+void SetValue(int, int);
+
+int area
+};
+
+int main()
+
+{
+
+}
